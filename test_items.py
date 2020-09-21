eff467b01586201e869a4c9e98f7e7b58951f738
henzhuk <genix@gmx.com>
2020-09-21 17:07:33 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ import time
 try: 
 
 def test_should_check_if_add_button_found(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
     button = browser.find_element_by_css_selector('.btn-add-to-basket')
