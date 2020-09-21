db041d1f5b2cd73cfa31afe8e3c32b7d37983585
henzhuk <genix@gmx.com>
2020-09-21 17:06:33 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,33 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC   
-
 import pytest
 import time
 
+try: 
 
-
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-def test_should_find_add_button(browser):
+def test_should_check_if_add_button_found(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(5)
-
+    time.sleep(30)
     button = browser.find_element_by_css_selector('.btn-add-to-basket')
-   
-    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), 'No such button!'
+    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), 'ADD button is abscent!'
 
-    #assert [type='submitt'], 'No such button!'
+finally:
 
-    #pagination_block = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(
-    #                (By.CSS_SELECTOR, "button.btn-add-to-basket")))
-
-
-time.sleep(5)
-
-
-
-
- 
+    time.sleep(5)
+    browser.quit()
 
